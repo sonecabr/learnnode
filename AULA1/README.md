3fd01c88323da7022c9a071441130bc4ed3b1db5
Andre Rocha <andre@konkerlabs.com>
2016-09-27 22:55:52 -0300

    aula1

---
+++ AULA1/README.md
@@ -0,0 +1,6 @@
+# Aula 1
+* Conceitos basicos de servidores http
+* O que é o nodejs
+* Casos de uso
+* npm
+* Hello word
---
+++ AULA1/server.js
@@ -0,0 +1,6 @@
+//importa a lib de http
+var http = require('http');
+
+http.createServer(function(request, response){
+    
+}).listen(8080);
