7cbfb0dc6d70bf12c84733c62989a1fd4fa7b6ed
Andre Rocha <andre@konkerlabs.com>
2016-09-30 00:30:31 -0300

    aula2 metodos

--- AULA2/server.js
+++ AULA2/server.js
@@ -1,10 +1,21 @@
 //importa a lib de http
 var http = require('http');
+var url = require('url');
+var qs = require('querystring');
+
 
 http.createServer(function(request, response){
     var method = request.method;
     if(method === 'GET'){
-        response.end('\nVoce fez uma requisicao do tipo GET\n');
+        urlParts = url.parse(request.url, true);
+        nome = urlParts.query.nome;
+        idade = parseInt(urlParts.query.idade);
+        if(idade > 30){
+            response.end('\nOI '+ nome +', Voce fez uma requisicao do tipo GET e voce e velho\n');
+        } else {
+            response.end('\nOI '+ nome +', Voce fez uma requisicao do tipo GET e voce e novo\n');
+        }
+
     }
     if(method === 'POST'){
         response.end('\nVoce fez uma requisição do tipo POST\n');
